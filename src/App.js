dcddf165c13c0f71c5ea68997f3a0411b3866b90
Gokkull04 <gokkull04@gmail.com>
2024-12-01 14:23:41 +0530

    login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-route
 import Dashboard from "./Dashboard";
 import Organize from "./Organize";
 import DevMaHomePage from "./HomePage"
+import Login from "./Login";
 
 
 
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<DevMaHomePage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/organize" element={<Organize />} />
       </Routes>
